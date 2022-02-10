13a0f5c32830b7c243a67e7979c04e7fd9bd9d83
Darren Garvey <darren.garvey@gmail.com>
2022-02-10 23:55:49 +0000

    chore: Make foam.sh able to run non-GPU version

--- foam.sh
+++ foam.sh
@@ -2,12 +2,15 @@
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-existingContainer="$(docker-compose ps -q foam)"
+# Run ./foam.sh docker-compose.cpu.yml if you don't have NVIDIA/X11 setup
+dockerfile="${1:-docker-compose.yml}"
+
+existingContainer="$(docker-compose -f ${dockerfile} ps -q foam)"
 
 if [[ "" == "$existingContainer" ]]; then
     # Allow opening windows from the docker container
     xhost +localhost
-    exec docker-compose run --rm foam
+    exec docker-compose -f ${dockerfile} run --rm foam
 else
     exec docker exec -it "$existingContainer" bash
 fi
